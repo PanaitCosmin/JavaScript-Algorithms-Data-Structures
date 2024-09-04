3cef15992197ffff4f0dd814a2c50735d64d610e
PanaitCosmin <cosmindaniel.panait@gmail.com>
2024-09-04 20:52:57 +0300

    Add top k frequent elements problem

---
+++ src/arrays-hashing/top-k-frequent-elements.js
@@ -0,0 +1,45 @@
+// LINK: https://leetcode.com/problems/top-k-frequent-elements/description/
+
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+/**
+ * 
+ * @param {number[]} nums - The array of integers.
+ * @param {number} k  - The number of top k frequent elements.
+ * @returns {number[]} - The `k` most frequent elements in `nums`
+ */
+function topKFrequent(nums, k) {
+    // Initialize a Map to store the number of occurrences for each number
+    const countNumbers = new Map()
+    // Initialize an Array where index represents frequency and value is a list of numbers with that frequency
+    const arr = new Array(nums.length + 1).fill(0)
+    // Initalize an Array to store the final result
+    const output = []
+
+    // Populate the frequency map with the count of each number in `nums`
+    nums.forEach((num) => {
+        const count = countNumbers.get(num) || 0
+        countNumbers.set(num, count + 1)
+    })
+
+    // Store in array numbers using index as their numbers of occurrences
+    for (let [num, occurrences] of countNumbers) {
+        const nums = arr[occurrences] || []
+        nums.push(num)
+        arr[occurrences] = nums
+    }
+
+    // Iterate in reverse order in arr to find the top k freq. elements
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (k < 0) break
+        if (arr[i]) {
+            for (let num of arr[i]) {
+                if (k < 1) break
+                output.push(num)
+                k--
+            }
+        }
+    }
+
+    return output
+}
